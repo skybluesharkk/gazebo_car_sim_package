d679112893e6e63ff562fdd5fd52a0ac8bbccca0
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2026-01-18 19:10:48 +0900

    feat: map에 장애물 추가 및 위치 설정

--- run_sim_server.sh
+++ run_sim_server.sh
@@ -2,7 +2,7 @@
 
 # 필수 환경 변수 설정
 export GZ_IP=127.0.0.1
-export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$(pwd)/models:$(pwd)/models/my_car_world
+export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$(pwd)/models:$(pwd)/models/my_car_world:$(pwd)/models/obstacles
 
 # macOS/M2 렌더링 에러 해결 핵심 변수
 export GZ_RENDERING_ENGINE_GUESS=ogre2
